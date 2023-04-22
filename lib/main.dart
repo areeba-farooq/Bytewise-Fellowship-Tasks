32534a356c1744340898d88b6934f327b5cc9d17
Flutter-Queen <meerahkhan5@gmail.com>
2023-04-22 21:01:19 +0500

    task9

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:bytewise_fellowship_tasks/Screens/get_started_screen.dart';
-import 'package:bytewise_fellowship_tasks/Screens/home_screen.dart';
-import 'package:bytewise_fellowship_tasks/Screens/login_screen.dart';
-import 'package:bytewise_fellowship_tasks/Screens/signup_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'Animations-task9/animation_task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,14 +15,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const GetStarted(),
-        '/login': (context) => const Login(),
-        '/signup': (context) => const Signup(),
-        '/home': (context) => const HomePage(),
-      },
-      // home: const GetStarted(),
+
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => const GetStarted(),
+      //   '/login': (context) => const Login(),
+      //   '/signup': (context) => const Signup(),
+      //   '/home': (context) => const HomePage(),
+      // },
+      home: const AnimatedBox(),
     );
   }
 }
